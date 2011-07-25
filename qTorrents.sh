2d5f88dfd4082e304278ea9fd517e0d12510be45
Ignacio <ibutler@gmail.com>
2011-07-25 14:31:04 -0300

    Added instructions to README file

--- qTorrents.sh
+++ qTorrents.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 
-#		qTorrrents.sh by ibutler (ibutler . gmail . com)
+#		qTorrrents.sh by ignaci0
 #		
 #		This program is free software; you can redistribute it and/or modify
 #		it under the terms of the GNU General Public License as published by
